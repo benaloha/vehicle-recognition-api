557a59326a5a6d747de7e9d433a9a9de6e486fab
benaloha <spammenicht7@gmail.com>
2021-02-28 12:00:41 +0100

    client

--- api_client.py
+++ api_client.py
@@ -1,11 +1,42 @@
 import requests
+import os
 
-filename = 'cars.jpg'
-file = open(filename, 'rb')
-files = {"image": file}
-res = requests.post(url = 'http://127.0.0.1:6000',
-                    files = files)
+path = '../../temp/2021-02-28 (copy)'
+with os.scandir(path) as entries:
+    print("analyse path: " + path)
 
-if res.status_code == 200:
-    for car in res.json()['cars']:
-        print(car)
+    try:
+        os.mkdir(path + "/cars")
+    except:
+        pass
+
+    for entry in entries:
+        print("analyse image: " + entry.name)
+        if entry.is_file():
+            if entry.name.endswith('.jpg') or entry.name.endswith('.thumb'):
+                file = open(entry, 'rb')
+                files={"image": file}
+                res=requests.post(url='http://127.0.0.1:6000',
+                                  files=files)
+
+                if res.status_code == 200:
+                    cars=res.json()['cars']
+                    if cars == []:
+                        print("no car in file, remove: " + entry.name)
+                        os.remove(entry)
+                    else:
+                        extra_fn = ''
+                        for car in cars:
+                            print(car)
+                            extra_fn.join(car.json()['make'])
+                        fn_parts = entry.name.split('.')
+                        newPath=path + '/cars/' + fn_parts[0] + extra_fn + fn_parts[1]
+                        print("move file to: " + newPath)
+                        os.rename(entry.path, newPath)
+            else:
+                print("no picture, remove: " + entry.name)
+                os.remove(entry)
+
+
+
+
